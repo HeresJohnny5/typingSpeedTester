ac4c57f57188de38b816119572067449b8f22565
John Erickson <ericksonjohn08@hotmail.com>
2017-08-06 22:42:14 -0400

    Add event listeners to testArea and resetButton. Add 3 functions which apply to each event listener.

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
-
 // Run a standard minute/second/hundredths timer:
-
+function spellCheck() {
+	let textEntered = testArea.value;
+	console.log(textEntered);
+}
 
 // Match the text entered with the provided text on the page:
 
-
 // Start the timer:
-
+function start() {
+	let textEnteredLength = (testArea.value.length) + 1;
+	console.log(textEnteredLength);
+}
 
 // Reset everything:
-
+function reset() {
+	console.log("Reset button has been pressed.");
+}
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener('keypress', start, false);
+testArea.addEventListener('keyup', spellCheck, false);
+resetButton.addEventListener('click', reset, false);
