df8be98f087c837cd725aaa09a4c73a4ce007a76
John Erickson <ericksonjohn08@hotmail.com>
2017-08-07 20:49:55 -0400

    Change function start conditional adding runTimer function per setInterval. Add runTimer function.

--- script.js
+++ script.js
@@ -4,19 +4,41 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = [0, 0, 0, 0];
+// Minute, Seconds, Hundredth of Seconds, Thousandths of Seconds
+
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 // Run a standard minute/second/hundredths timer:
+function runTimer() {
+	let currentTime = timer[0] + ':' + timer[1] + ':' + timer[2];
+	theTimer.innerHTML = currentTime;
+	timer[3]++;
+	
+	timer[0] = Math.floor((timer[3]/100)/60);
+	// Minutes = Thousandth of a Second/100 equals seconds/60 equals minutes
+	
+	timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+	/* Seconds = (Thousandth of a Second/100 equals seconds) - (Minute * 60). Every 60 seconds the value will return to 0. */
+	
+	timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+	/* Hundredth of a Second = (Thousandth of a Second - (Seconds * 100) - (Minute * 6000) */
+	
+}
+
+// Match the text entered with the provided text on the page:
 function spellCheck() {
 	let textEntered = testArea.value;
 	console.log(textEntered);
 }
 
-// Match the text entered with the provided text on the page:
-
 // Start the timer:
 function start() {
-	let textEnteredLength = (testArea.value.length) + 1;
+	let textEnteredLength = testArea.value.length;
+	
+	if(textEnteredLength === 0) {
+		setInterval(runTimer, 10);
+	}
 	console.log(textEnteredLength);
 }
 
